f5de840cb377c46d50829442af8b95c09268feea
Ananya <ananyakaushik48@gmail.com>
2023-04-04 23:26:36 +0530

    login complete

--- src/routes/api/auth/callback/+server.ts
+++ src/routes/api/auth/callback/+server.ts
@@ -1,19 +1,19 @@
 import type { RequestHandler } from "./$types";
-import { error } from '@sveltejs/kit';
-import { SPOTIFY_APP_CLIENT_ID, SPOTIFY_APP_CLIENT_SECRET } from '$env/static/private';
+import { redirect,error } from '@sveltejs/kit';
+import { BASE_URL, SPOTIFY_APP_CLIENT_ID, SPOTIFY_APP_CLIENT_SECRET } from '$env/static/private';
 
-export const GET: RequestHandler = ({ url, cookies }) => {
+export const GET: RequestHandler = async ({ url, cookies }) => {
     const code = url.searchParams.get('code') || null;
     const state = url.searchParams.get('state') || null;
 
     const storedState = cookies.get('spotify_auth_state') || null;
     const storedChallengeVerifier = cookies.get('spotify_auth_challenge_verifier') || null;
 
-    if (state !== storedState) {
+    if (state == null || state !== storedState) {
         throw error(400, 'State Mismatch!');
     }
 
-    const response = await fetch('https://accounts.spotify.com/api/tokens', {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -23,7 +23,20 @@ export const GET: RequestHandler = ({ url, cookies }) => {
             code: code || '',
             redirect_uri: `${BASE_URL}/api/auth/callback`,
             grant_type: 'authorization_code',
-            code_verifier:
+            code_verifier: storedChallengeVerifier || '',
+            client_id: SPOTIFY_APP_CLIENT_ID
         })
     })
+    const responseJSON = await response.json();
+
+    if (responseJSON.error) {
+        console.log(responseJSON)
+        throw error(400, responseJSON.error_description);
+    }
+    cookies.delete('spotify_auth_state')
+    cookies.delete('spotify_auth_challenge_verifier')
+    cookies.set('refresh_token', responseJSON.refresh_token, { path: '/' });
+    cookies.set('access_token', responseJSON.access_token, { path: '/' });
+
+    throw redirect(303, '/');
 }
